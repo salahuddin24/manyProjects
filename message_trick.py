90ca8cf849e526c02f576f54a86c6dd65fac9a56
SALAH UDDIN <155283692+salahuddin24@users.noreply.github.com>
2024-11-08 11:53:53 +0600

    Update message_trick.py

--- message_trick.py
+++ message_trick.py
@@ -1,9 +1,11 @@
 # firstly install : pip install pyautogui
 import pyautogui
 import time
-while True:
+message = 5
+while message > 0:
     time.sleep(1)
     pyautogui.typewrite("How are you?")
     time.sleep(1)
     pyautogui.press("enter")
+    message = message - 1
 
